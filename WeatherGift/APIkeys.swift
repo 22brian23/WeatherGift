1a890d37c7f6527373a36ff5a50683247c7d2d33
Brian Bowers <22bbowers23@gmail.com>
2020-04-13 20:15:34 -0700

    with.gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Brian Bowers on 4/13/20.
+//  Copyright © 2020 Brian Bowers. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyANhMPUSTXl-6hnhLxhK5xx5a6vA0_7CYA"
+}
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Brian Bowers on 4/7/20.
+//  Copyright © 2020 Brian Bowers. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    
+}
